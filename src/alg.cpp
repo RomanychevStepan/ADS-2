b6d499abe37bd7285ee437709e2532e5c2061433
RomanychevStepan <124895851+RomanychevStepan@users.noreply.github.com>
2023-03-11 19:52:39 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -28,24 +28,24 @@ double calcItem(double x, uint16_t n) {
 }
 
 double expn(double x, uint16_t count) {
-    double sum;
-    for (uint16_t i = 0; i <=count; i++) {
+    double sum = 1;
+    for (uint16_t i = 1; i <=count; i++) {
         sum += calcItem(x, i);
     }
     return sum;
 }
 
 double sinn(double x, uint16_t count) {
-    double sum;
-    for (uint16_t i = 0; i <=count; i++) {
+    double sum = x;
+    for (uint16_t i = 2; i <=count; i++) {
         sum = sum + calcItem(x, 2 * i - 1) * pown(-1, i - 1);
     }
     return sum;
 }
 
 double cosn(double x, uint16_t count) {
-    double sum;
-    for (uint16_t i = 0; i <= count; i++) {
+    double sum = 1;
+    for (uint16_t i = 2; i <= count; i++) {
         sum = sum + calcItem(x, 2 * i - 2) * pown(-1, i - 1);
     }
     return sum;
